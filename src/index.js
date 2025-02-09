639a12db924fd71c2c2d74ca804b99dfcb0fb151
Chad Harper <chadharper811@gmail.com>
2025-02-08 19:21:36 -0600

    practice problem 5

--- src/index.js
+++ src/index.js
@@ -4,17 +4,51 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const JSX = (
-  <div>
-    <h1>This is a block of JSX</h1>
-    <p>Here's a subtitle</p>
-    {/* Comment */}
-  </div>
-);
+
+const TypesOfFruit = () => {
+  return (
+    <div>
+      <h2>Fruits:</h2>
+      <ul>
+        <li>Apples</li>
+        <li>Blueberries</li>
+        <li>Strawberries</li>
+        <li>Bananas</li>
+      </ul>
+    </div>
+  );
+};
+
+const Fruits = () => {
+  return (
+    <div>
+      { /* Change code below this line */ }
+      <TypesOfFruit />
+      { /* Change code above this line */ }
+    </div>
+  );
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        { /* Change code below this line */ }
+        <Fruits />
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <TypesOfFood />
 );
 
 // If you want to start measuring performance in your app, pass a function
