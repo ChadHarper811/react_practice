f61f1d48c65d89c8749d22d40412b583eea7e007
Chad Harper <chadharper811@gmail.com>
2025-02-08 19:17:31 -0600

    testing problem 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import React from 'react';
 
-function App() {
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+      </div>
+    );
+  }
+};
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         HELLO WORLD!
+        <ParentComponent />
       </header>
     </div>
   );
