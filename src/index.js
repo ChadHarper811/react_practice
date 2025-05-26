d65fcfe6687041add97e9aacf5d342653369aca2
Chad Harper <chadharper811@gmail.com>
2025-05-26 12:09:36 -0500

    added ShoppingCart component for practice problem

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Calendar from './Calendar';
 import ToDo from './ToDo';
+import ShoppingCart from './ShoppingCart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
     <App />
     <Calendar />
     <ToDo />
+    <ShoppingCart />
   </React.StrictMode>
 );
 
