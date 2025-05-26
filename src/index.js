dca2c3fcb4e4bb2879f7097269c87141f672292e
Chad Harper <chadharper811@gmail.com>
2025-05-26 11:52:23 -0500

    copied ToDo component from practice problem

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Calendar from './Calendar';
+import ToDo from './ToDo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Calendar />
+    <ToDo />
   </React.StrictMode>
 );
 
