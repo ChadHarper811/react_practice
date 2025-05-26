701b027c1fa54c4bbd8320d2e88563a63ce6e5a5
Chad Harper <chadharper811@gmail.com>
2025-05-26 12:12:05 -0500

    solution added to shoppingcart

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -2,8 +2,11 @@ const ShoppingCart = (props) => {
   return (
     <div>
       <h1>Shopping Cart Component</h1>
+      <p>{props.items}</p>
     </div>
   )
 };
+
+ShoppingCart.defaultProps = { items: 0}
 
 export default ShoppingCart;
