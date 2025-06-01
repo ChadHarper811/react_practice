70d61554dfc2c2a046b56914ac24ce22c561c1be
Chad Harper <chadharper811@gmail.com>
2025-06-01 11:54:27 -0500

    changed shoppingcart to match next problems starting code

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -2,20 +2,22 @@ import React from "react";
 
 const Items = (props) => {
   return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
-}
+};
+
+// Change code below this line
+
+// Change code above this line
 
 Items.defaultProps = {
   quantity: 0
-}
+};
 
 class ShoppingCart extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    { /* Change code below this line */ }
-    return <Items quantity={10}/>
-    { /* Change code above this line */ }
+    return <Items />
   }
 };
 
